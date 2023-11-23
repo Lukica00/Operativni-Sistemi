5ee65dd9e1df442cc72f8447a69820bd8ef21e5b
Luka Lazarević <lukalazarevic00@gmail.com>
2023-11-23 21:12:53 +0100

    free mem

--- z4.15a.c
+++ z4.15a.c
@@ -2,22 +2,27 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
-#include<sys/wait.h>
+#include <sys/wait.h>
 int main(int argc, char **argv)
 {
-	char **vgra = (char**) malloc((argc)*sizeof(char*));
+	char **vgra = (char **)malloc((argc) * sizeof(char *));
 	for (int i = 2; i < argc; i++)
 	{
 		vgra[argc - i - 1] = (char *)malloc(strlen(argv[i]) * sizeof(char));
 		strcpy(vgra[argc - i - 1], argv[i]);
 	}
-	vgra[argc - 2]=NULL;
-	if(fork()==0)
+	vgra[argc - 2] = NULL;
+	if (fork() == 0)
 	{
-		printf("%d \n",execv(argv[1], vgra));
+		printf("%d \n", execv(argv[1], vgra));
 	}
 	else
 	{
 		wait(NULL);
 	}
+	for (int i = 2; i < argc; i++)
+	{
+		free(vgra[argc - i - 1]);
+	}
+	free(vgra);
 }
